f74164456f426755983ece78762ed428c3e6b4fd
chp567 <73819652+chp567@users.noreply.github.com>
2023-06-07 14:32:38 +0200

    register change for PL

--- front/src/components/LoginContainer.tsx
+++ front/src/components/LoginContainer.tsx
@@ -1,10 +1,14 @@
-import { FunctionComponent, useCallback } from "react";
+import { FunctionComponent, useCallback, useState } from "react";
 import LoginFormContainer from "./LoginFormContainer";
 import { useNavigate } from "react-router-dom";
 import "../styles/compo/LoginContainer.css";
+
 const LoginContainer: FunctionComponent = () => {
   const navigate = useNavigate();
-
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  
   const onButtonContainerClick = useCallback(() => {
     navigate("/dashboard");
   }, [navigate]);
@@ -19,20 +23,22 @@ const LoginContainer: FunctionComponent = () => {
         <LoginFormContainer
           loginEmail="Email"
           loginPassword="Logging in will help you access actions such as viewing video feeds. To log in, please enter your username below."
-          loginEmailAddress="charlus@perrus.com"
-          propDisplay="none"
+          loginEmailAddress={email}
+          onChange={(value) => setEmail(value)}
         />
         <LoginFormContainer
           loginEmail="Password"
           loginPassword="Logging in will help you access actions such as viewing video feeds. To log in, please enter your username below."
-          loginEmailAddress="********************"
+          loginEmailAddress={password}
           propDisplay="none"
+          onChange={(value) => setPassword(value)}
         />
         <LoginFormContainer
           loginEmail="Password 2: Electric Boogaloo"
           loginPassword="Please type your password a second time so you can make sure to have typed it right."
-          loginEmailAddress="********************"
+          loginEmailAddress={password2}
           propDisplay="inline-block"
+          onChange={(value) => setPassword2(value)}
         />
       </div>
       <div className="button1" onClick={onButtonContainerClick}>
