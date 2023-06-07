18144f51b1bc5f33de633adbce7936f5661c7542
Karitham <kar@karitham.dev>
2023-06-07 13:58:55 +0000

    fix login page

--- front/src/components/LoginFormContainer.tsx
+++ front/src/components/LoginFormContainer.tsx
@@ -1,49 +1,50 @@
-import { FunctionComponent, useMemo } from "react";
-import CSS, { Property } from "csstype";
-import "../styles/compo/LoginFormContainer.css";
+import { FunctionComponent, useMemo } from 'react'
+import CSS, { Property } from 'csstype'
+import '../styles/compo/LoginFormContainer.css'
 
 type LoginFormContainerType = {
-  loginEmail?: string;
-  loginPassword?: string;
-  loginEmailAddress?: string;
+  loginEmail?: string
+  loginPassword?: string
+  loginEmailAddress?: string
 
   /** Style props */
-  propDisplay?: Property.Display;
-  onChange?: (value: string) => void;
-};
+  propDisplay?: Property.Display
+  onChange?: (value: string) => void
+}
 
 const LoginFormContainer: FunctionComponent<LoginFormContainerType> = ({
-  loginEmail,
-  loginPassword,
-  loginEmailAddress,
+  loginEmail: title,
+  loginPassword: description,
+  loginEmailAddress: value,
   propDisplay,
-  onChange,
+  onChange
 }) => {
   const loggingInWillStyle: CSS.Properties = useMemo(() => {
     return {
-      display: propDisplay,
-    };
-  }, [propDisplay]);
+      display: propDisplay
+    }
+  }, [propDisplay])
 
   return (
-    <div className="input5">
-      <div className="login3">{loginEmail}</div>
-      <div className="logging-in-will1" style={loggingInWillStyle}>
-        {loginPassword}
-      </div>
-      <div className="input6">
-        <input
-          type="text"
-          className="lessgo-we-on2"
-          value={loginEmailAddress}
-          onChange={(e) => onChange?.(e.target.value)}
-        />
-        <div className="magnifyingglass3">
-          <img className="vector-icon18" alt="" src="/vector7.svg" />
-        </div>
-      </div>
+    <div className="login-wrapper">
+      <label htmlFor={title} className="login-label">
+        {title}
+      </label>
+      {description && (
+        <p className="login-description" style={loggingInWillStyle}>
+          {description}
+        </p>
+      )}
+      <input
+        name={title}
+        title={title}
+        type="text"
+        className="login-input"
+        value={value}
+        onChange={e => onChange?.(e.target.value)}
+      />
     </div>
-  );
-};
+  )
+}
 
-export default LoginFormContainer;
+export default LoginFormContainer
--- front/src/components/LoginFormFilterContainer.tsx
+++ front/src/components/LoginFormFilterContainer.tsx
@@ -1,36 +1,30 @@
-import { FunctionComponent, useCallback, useState } from "react";
-import LoginFormContainer from "./LoginFormContainer";
-import { useNavigate } from "react-router-dom";
-import "../styles/compo/LoginFormFilterContainer.css";
+import { FunctionComponent, useCallback, useState } from 'react'
+import LoginFormContainer from './LoginFormContainer'
+import { useNavigate } from 'react-router-dom'
+import '../styles/compo/LoginFormFilterContainer.css'
 
 const LoginFormFilterContainer: FunctionComponent = () => {
-  const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  
+  const navigate = useNavigate()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   const onButtonContainerClick = useCallback(() => {
-    navigate("/dashboard");
-  }, [navigate]);
+    navigate('/dashboard')
+  }, [navigate])
 
   const onDontHaveAnClick = useCallback(() => {
-    navigate("/register");
-  }, [navigate]);
+    navigate('/register')
+  }, [navigate])
 
   return (
     <div className="inputs-group">
       <div className="inputs1">
-        <LoginFormContainer
-          loginEmail="Email"
-          loginPassword="Logging in will help you access actions such as viewing video feeds. To log in, please enter your username below."
-          loginEmailAddress={email}
-          onChange={(value) => setEmail(value)}
-        />
+        <LoginFormContainer loginEmail="Email" loginEmailAddress={email} onChange={value => setEmail(value)} />
         <LoginFormContainer
           loginEmail="Password"
-          loginPassword="Logging in will help you access actions such as viewing video feeds. To log in, please enter your username below."
           loginEmailAddress={password}
           propDisplay="none"
-          onChange={(value) => setPassword(value)}
+          onChange={value => setPassword(value)}
         />
       </div>
       <div className="button2" onClick={onButtonContainerClick}>
@@ -40,7 +34,7 @@ const LoginFormFilterContainer: FunctionComponent = () => {
         Don’t have an account?
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LoginFormFilterContainer;
+export default LoginFormFilterContainer
