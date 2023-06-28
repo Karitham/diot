d2091b843325b274862a0b8762444151664f10e5
Karitham <kar@karitham.dev>
2023-06-28 17:06:43 +0200

    fix npm run dev && dashboard video size

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,24 +5,32 @@ import react from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    tsConfigPaths(),
-    react(),
-    electron([
-      {
-        // Main-Process entry file of the Electron App.
-        entry: 'electron/main.ts'
-      },
-      {
-        entry: 'electron/preload.ts',
-        onstart(options) {
-          // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
-          // instead of restarting the entire Electron App.
-          options.reload()
-        }
-      }
-    ]),
-    renderer()
-  ]
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [tsConfigPaths(), react(), renderer()]
+    }
+  } else if (command === 'build') {
+    return {
+      plugins: [
+        tsConfigPaths(),
+        react(),
+        electron([
+          {
+            // Main-Process entry file of the Electron App.
+            entry: 'electron/main.ts'
+          },
+          {
+            entry: 'electron/preload.ts',
+            onstart(options) {
+              // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
+              // instead of restarting the entire Electron App.
+              options.reload()
+            }
+          }
+        ]),
+        renderer()
+      ]
+    }
+  }
 })
