042f36f250f00afaeff6035bd7c7828a54707cd2
Karitham <kar@karitham.dev>
2023-06-29 17:08:50 +0200

    planning

--- front/src/components/AdminPlanningContainer.tsx
+++ front/src/components/AdminPlanningContainer.tsx
@@ -10,33 +10,48 @@ import MondayComponent from './WeekBar'
 import AdminDayContainer from './AdminDayContainer'
 import AdminHoursContainer from './AdminHoursContainer'
 
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
 const AdminPlanningContainer: FunctionComponent = memo(() => {
+  const [startDay, setStartDay] = useState(days[0])
+  const [endDay, setEndDay] = useState(days[4])
+  const [startTime, setStartTime] = useState('07:00')
+  const [endTime, setEndTime] = useState('18:00')
 
-  const [startDay, setStartDay] = useState('')
-  const [endDay, setEndDay] = useState('')
-
-  const handleStartDaySelection = (selectedDay: string) => {
-    setStartDay(selectedDay)
-  }
-
-  const handleEndDaySelection = (selectedDay: string) => {
-    setEndDay(selectedDay)
-  }
+  const handleStartDaySelection = (selectedDay: string) => setStartDay(selectedDay)
+  const handleEndDaySelection = (selectedDay: string) => setEndDay(selectedDay)
 
   const onResetClick = () => {
-    setStartDay('') // Réinitialise la valeur du premier dropdown
-    setEndDay('') // Réinitialise la valeur du deuxième dropdown
+    setStartDay(days[0])
+    setEndDay(days[4])
   }
 
-  const [startTime, setStartTime] = useState('')
-  const [endTime, setEndTime] = useState('')
+  const handleStartTimeChange = (value: string) => setStartTime(value)
+  const handleEndTimeChange = (value: string) => setEndTime(value)
 
-  const handleStartTimeChange = (value: string) => {
-    setStartTime(value)
+  // disable from monday 7:00
+  const dayDisableStart = (currentDay: string, startDay: string, startTime: string): number => {
+    // if we are after the start day or now is the start day
+    if (days.indexOf(currentDay) >= days.indexOf(startDay)) {
+      return parseT(startTime) // number of minutes from 00:00 to startTime
+    }
+
+    return parseT(endTime) // number of minutes from 00:00 to endTime
   }
 
-  const handleEndTimeChange = (value: string) => {
-    setEndTime(value)
+  const parseT = (t: string): number => {
+    const [hours, minutes] = t.split(':')
+    return parseInt(hours, 10) * 60 + parseInt(minutes, 10)
+  }
+
+  // disable from friday 18:00
+  const dayDisableEnd = (currentDay: string, endDay: string, endTime: string): number => {
+    // if we are before the end day or now is the end day
+    if (days.indexOf(currentDay) <= days.indexOf(endDay)) {
+      return parseT(endTime)
+    }
+
+    return parseT(startTime)
   }
 
   return (
@@ -50,13 +65,41 @@ const AdminPlanningContainer: FunctionComponent = memo(() => {
               <div className="preview-the-dayss">Preview the days it’s on</div>
             </div>
             <div className="instance-parents">
-              <MondayComponent dayOfWeek="Monday" />
-              <MondayComponent dayOfWeek="Tuesday" />
-              <MondayComponent dayOfWeek="Wednesday" />
-              <MondayComponent dayOfWeek="Thursday" />
-              <MondayComponent dayOfWeek="Friday" />
-              <MondayComponent dayOfWeek="Saturday" />
-              <MondayComponent dayOfWeek="Sunday" />
+              <MondayComponent
+                dayOfWeek="Monday"
+                disabledEnd={dayDisableEnd('Monday', endDay, endTime)}
+                disabledStart={dayDisableStart('Monday', startDay, startTime)}
+              />
+              <MondayComponent
+                dayOfWeek="Tuesday"
+                disabledEnd={dayDisableEnd('Tuesday', endDay, endTime)}
+                disabledStart={dayDisableStart('Tuesday', startDay, startTime)}
+              />
+              <MondayComponent
+                dayOfWeek="Wednesday"
+                disabledEnd={dayDisableEnd('Tuesday', endDay, endTime)}
+                disabledStart={dayDisableStart('Wednesday', startDay, startTime)}
+              />
+              <MondayComponent
+                dayOfWeek="Thursday"
+                disabledEnd={dayDisableEnd('Thursday', endDay, endTime)}
+                disabledStart={dayDisableStart('Thursday', startDay, startTime)}
+              />
+              <MondayComponent
+                dayOfWeek="Friday"
+                disabledEnd={dayDisableEnd('Friday', endDay, endTime)}
+                disabledStart={dayDisableStart('Friday', startDay, startTime)}
+              />
+              <MondayComponent
+                dayOfWeek="Saturday"
+                disabledEnd={dayDisableEnd('Saturday', endDay, endTime)}
+                disabledStart={dayDisableStart('Saturday', startDay, startTime)}
+              />
+              <MondayComponent
+                dayOfWeek="Sunday"
+                disabledEnd={dayDisableEnd('Sunday', endDay, endTime)}
+                disabledStart={dayDisableStart('Sunday', startDay, startTime)}
+              />
             </div>
           </div>
           <div className="fieldss">
@@ -84,16 +127,8 @@ const AdminPlanningContainer: FunctionComponent = memo(() => {
                   </div>
                 </div>
                 <div className="selects">
-                  <AdminHoursContainer
-                    name="Start"
-                    value={startTime}
-                    onInput={handleStartTimeChange}
-                  />
-                  <AdminHoursContainer
-                    name="End"
-                    value={endTime}
-                    onInput={handleEndTimeChange}
-                  />
+                  <AdminHoursContainer name="Start" value={startTime} onInput={handleStartTimeChange} />
+                  <AdminHoursContainer name="End" value={endTime} onInput={handleEndTimeChange} />
                 </div>
               </div>
             </div>
--- front/src/components/WeekBar.tsx
+++ front/src/components/WeekBar.tsx
@@ -1,84 +1,39 @@
-import React from 'react';
-import '../styles/compo/WeekBar.css';
+import React, { useEffect, useRef } from 'react'
+import '../styles/compo/WeekBar.css'
 
 interface MondayComponentProps {
-  dayOfWeek: string;
+  dayOfWeek: string
+  disabledStart: number
+  disabledEnd: number
 }
 
-const MondayComponent: React.FC<MondayComponentProps> = ({ dayOfWeek }) => {
+const MondayComponent: React.FC<MondayComponentProps> = (props: MondayComponentProps) => {
+  const ref = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    const canvas = ref.current
+    if (!canvas) return
+
+    const ctx = canvas.getContext('2d')
+    if (!ctx) return
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    ctx.fillStyle = '#2b4162'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    ctx.fillStyle = '#d9d9d9'
+    ctx.fillRect(props.disabledStart, 0, props.disabledEnd - props.disabledStart, canvas.height)
+  }, [props.disabledStart, props.disabledEnd])
+
   return (
     <div className="monday-parent">
-      <div className="monday">{dayOfWeek}</div>
+      <div className="monday">{props.dayOfWeek}</div>
       <div className="planning2">
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child17" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
-        <div className="planning-child" />
+        <canvas className="canvas-full" ref={ref} height={10} width={24 * 60}></canvas>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default MondayComponent;
+export default MondayComponent
