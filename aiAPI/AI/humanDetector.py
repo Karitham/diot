ac158a6ded67bb213424e9f855c79cf30e409bca
Xav1204 <xavier.thellier@viacesi.fr>
2023-06-29 17:11:22 +0200

    test ai

--- aiAPI/AI/humanDetector.py
+++ aiAPI/AI/humanDetector.py
@@ -3,6 +3,30 @@ import numpy as np
 import datetime
 from AI.motherDetector import MotherDetection
 
+class HumanDetection:
+    def __init__(self, id_iot, image):
+        self.id_iot = id_iot
+        self.image = image
+
+    def detecter_human(self):
+        # Vérifier si l'image n'est pas vide
+        if self.image is None:
+            return
+
+        timestamp = datetime.datetime.now()
+        formatted_timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        try:
+            json_data = MotherDetection.dataSchema.human_schema(self.id_iot, formatted_timestamp)
+        except Exception as e:
+            print("Erreur lors de l'appel de la fonction human_schema dans la classe DataSchema", str(e))
+        
+        try:
+            MotherDetection.redisPublisher.publish_alert(json_data)
+        except Exception as e:
+            print("Erreur lors de l'appel de la fonction publish_alert dans la classe RedisPublisher", str(e))
+
+
+"""
 # Charger le modèle de détection de corps entiers (HOG + SVM)
 body_cascade = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
 
@@ -29,7 +53,7 @@ class HumanDetection:
             try:
                 json_data = MotherDetection.dataSchema.human_schema(self.id_iot, formatted_timestamp)
             except Exception as e:
-                print("Erreur lors de l'appel de la fonction flooding_schema dans la classe DataSchema", str(e))
+                print("Erreur lors de l'appel de la fonction human_schema dans la classe DataSchema", str(e))
             
             try:
                 MotherDetection.redisPublisher.publish_alert(json_data)
@@ -41,4 +65,4 @@ class HumanDetection:
         # Vérification si aucun corps n'est détecté et que le message a déjà été imprimé
         elif len(bodies) == 0 and not self.humain_disparu:
             self.humain_present = False
-            self.humain_disparu = True
+            self.humain_disparu = True"""
